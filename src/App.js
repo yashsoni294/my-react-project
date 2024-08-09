70171f24c712e8d7b949275df44751d0f6e5e766
Yash Soni <yashmukeshsoni@gmail.com>
2024-08-09 22:41:44 +0530

    yash

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <h1>this as dfj;lfjlfjdfolkokdjflldfj;ls</h1>
+      <About></About>
     </div>
   );
 }
